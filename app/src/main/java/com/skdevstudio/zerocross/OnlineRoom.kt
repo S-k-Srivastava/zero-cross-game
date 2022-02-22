24a1a466ab2c2e98540bcd1c454f542682d6ebc0
Saurav Srivastava <9s8k7s@gmail.com>
2022-02-22 11:15:51 +0530

    Added Firebase Authentication

---
+++ app/src/main/java/com/skdevstudio/zerocross/OnlineRoom.kt
@@ -0,0 +1,14 @@
+package com.skdevstudio.zerocross
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class OnlineRoom : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_online_room)
+
+
+
+    }
+}
